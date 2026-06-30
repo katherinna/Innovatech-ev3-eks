db1ae5bbcb78a3cfc269eed71526ad67eeb51ae0
katherinna <k.candia@duocuc.cl>
2026-06-29 22:34:01 -0400

    Cambio para prueba de pipelines

--- frontend/src/componentes/Layouts/Navbar.jsx
+++ frontend/src/componentes/Layouts/Navbar.jsx
@@ -2,7 +2,7 @@ function Navbar() {
   return (
     <nav className="rounded-xl w-[250px] min-h-[880px] bg-teal-600 text-white sticky top-0 p-4 m-4">
       {/* Logo o título */}
-      <h2 className="text-xl font-bold mb-8">Despachos Dashboard</h2>
+      <h2 className="text-xl font-bold mb-8">Despachos</h2>
 
       {/* Menú de navegación */}
       <ul className="space-y-3">
@@ -11,7 +11,7 @@ function Navbar() {
             href="#"
             className="block font-bold py-2 px-3 hover:bg-teal-700 rounded"
           >
-            Usuarios
+            Usuario
           </a>
         </li>
         <li>
